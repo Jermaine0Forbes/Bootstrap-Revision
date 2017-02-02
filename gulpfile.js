8aa10e59868677689ad2926c780e4f583796e128
Jermaine Forbes <jermaine0forbes@gmail.com>
2017-02-02 07:31:28 -0500

    Added css and scss directories

---
+++ gulpfile.js
@@ -0,0 +1,15 @@
+var gulp = require('gulp');
+var sass = require('gulp-sass');
+
+
+gulp.task('sass', function(){
+
+  gulp.src('./scss/bootstrap-revision.scss')
+  .pipe(sass())
+  .pipe(gulp.dest('./css'));
+})
+
+gulp.task('watch',['sass'],function(){
+
+gulp.watch('./scss/*.scss',['sass']);
+})
