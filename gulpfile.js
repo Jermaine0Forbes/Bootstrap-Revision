d38f8ea945a041dd975ee7900d05b8d807356898
Jermaine Forbes <jermaine0forbes@gmail.com>
2018-09-06 19:04:57 -0400

    created: iso concierge folder/css, and updated the gulp file

--- gulpfile.js
+++ gulpfile.js
@@ -10,20 +10,23 @@ gulp.task('sass', function(){
   gulp.src('./scss/revision/bootstrap-revision.scss')
   .pipe(sass())
   .pipe(gulp.dest('./css'));
+
+
 })
 
 gulp.task('utility', function(){
 
-  gulp.src('./scss/utilities/utility.scss')
+  gulp.src(['./scss/utilities/utility.scss','./scss/utilities/iso-concierge/iso.scss'])
   .pipe(sass())
   .pipe(csscomb())
   .pipe(gulp.dest('./css'));
+
 })
 
 
 gulp.task('watch',['sass', 'utility'],function(){
 
 gulp.watch('./scss/revision/*.scss',['sass']);
-gulp.watch('./scss/utilities/*.scss',['utility']);
+gulp.watch(['./scss/utilities/*.scss','./scss/utilities/iso-concierge/*.scss'],['utility']);
 
 })
