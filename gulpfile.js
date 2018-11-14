aa37c43905c142ef7a4f8ae852fcf98a971c0d71
Jermaine Forbes <jermaine0forbes@gmail.com>
2018-11-14 10:56:11 -0500

    created: clean folder; updated:sesh.css

--- gulpfile.js
+++ gulpfile.js
@@ -1,8 +1,16 @@
 var gulp = require('gulp');
 var sass = require('gulp-sass');
 var csscomb = require('gulp-csscomb');
+const minify = require('gulp-minify');
+var minifyPipeline = require('pipeline-minify-css');
+var cleanCSS = require('gulp-clean-css');
+minifyPipeline.config = {
+  addSourceMaps: false,
+  concatCSS:false,
+  concat: false
+}
 
-gulp.task('default',['sass']);
+gulp.task('default',['clean']);
 
 
 gulp.task('sass', function(){
@@ -20,8 +28,19 @@ gulp.task('utility', function(){
   './scss/utilities/iso-concierge/iso.scss',
   './scss/utilities/sesh-business-funding/sesh.scss'])
   .pipe(sass())
-  .pipe(csscomb())
+  .pipe(cleanCSS())
   .pipe(gulp.dest('./css'));
+
+})
+
+gulp.task('clean', function(){
+
+  gulp.src(['./scss/utilities/utility.scss',
+  './scss/utilities/iso-concierge/iso.scss',
+  './scss/utilities/sesh-business-funding/sesh.scss'])
+  .pipe(sass())
+  .pipe(csscomb())
+  .pipe(gulp.dest('./css/clean'));
 
 })
 
